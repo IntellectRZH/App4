093bc2db09c1eaa094b6d076f4e50e067c95a1c5
Владимир Рожнов <sobaka@gpntb.ru>
2020-04-11 20:26:12 +0300

    Добавьте файлы проекта.

---
+++ App4/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App4
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage())
+            {
+                BarBackgroundColor = Color.FromHex("3f51b5"),
+                HeightRequest = 40,
+                BarTextColor = Color.FromHex("F1F8E9"),
+                StyleId = "Bolt"
+            };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ App4/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App4
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ToolbarItem About = new ToolbarItem()
+            {
+                Text = "О программе",
+                Order = ToolbarItemOrder.Secondary,
+                Priority = 2
+            };
+            ToolbarItems.Add(About);
+            About.Clicked += About_Clicked;
+
+            ToolbarItem Garbage = new ToolbarItem()
+            {
+                Text = "Помойка",
+                Order = ToolbarItemOrder.Secondary,
+                Priority = 1
+            };
+            ToolbarItems.Add(Garbage);
+        }
+
+        private async void About_Clicked(object sender, EventArgs e) => await DisplayAlert("О программе", "Сделано\nРожновым Владимиром\nE-mail: Rozhnov.vova@gmail.com", "Закрыть");
+       
+
+        private void StartButtnForCloseApp_Clicked(object sender, EventArgs e) => Environment.Exit(0);
+
+    }
+}
